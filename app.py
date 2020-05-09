4a74ccd69cb8e824cd3f6e8b8d7de2f597ce922a
Adil Moujahid <adil.mouja@gmail.com>
2020-05-10 01:51:15 +0200

    Ignore empty houses in the simulation and similarity ratio calculation

--- app.py
+++ app.py
@@ -34,31 +34,33 @@ class Schelling:
     def run(self):
         for (row, col), value in np.ndenumerate(self.city):
             race = self.city[row, col]
-            neighborhood = self.city[row-self.n_neighbors:row+self.n_neighbors, col-self.n_neighbors:col+self.n_neighbors]
-            neighborhood_size = np.size(neighborhood)
-            n_empty_houses = len(np.where(neighborhood == 0)[0])
-            if neighborhood_size != n_empty_houses + 1:
-                n_similar = len(np.where(neighborhood == race)[0]) - 1
-                similarity_ratio = n_similar / (neighborhood_size - n_empty_houses - 1.)
-                is_unhappy = (similarity_ratio < self.similarity_threshold)
-                if is_unhappy:
-                    empty_houses = list(zip(np.where(self.city == 0)[0], np.where(self.city == 0)[1]))
-                    random_house = random.choice(empty_houses)
-                    self.city[random_house] = race
-                    self.city[row,col] = 0
+            if race != 0:
+                neighborhood = self.city[row-self.n_neighbors:row+self.n_neighbors, col-self.n_neighbors:col+self.n_neighbors]
+                neighborhood_size = np.size(neighborhood)
+                n_empty_houses = len(np.where(neighborhood == 0)[0])
+                if neighborhood_size != n_empty_houses + 1:
+                    n_similar = len(np.where(neighborhood == race)[0]) - 1
+                    similarity_ratio = n_similar / (neighborhood_size - n_empty_houses - 1.)
+                    is_unhappy = (similarity_ratio < self.similarity_threshold)
+                    if is_unhappy:
+                        empty_houses = list(zip(np.where(self.city == 0)[0], np.where(self.city == 0)[1]))
+                        random_house = random.choice(empty_houses)
+                        self.city[random_house] = race
+                        self.city[row,col] = 0
 
     def get_mean_similarity_ratio(self):
         count = 0
         similarity_ratio = 0
         for (row, col), value in np.ndenumerate(self.city):
             race = self.city[row, col]
-            neighborhood = self.city[row-self.n_neighbors:row+self.n_neighbors, col-self.n_neighbors:col+self.n_neighbors]
-            neighborhood_size = np.size(neighborhood)
-            n_empty_houses = len(np.where(neighborhood == 0)[0])
-            if neighborhood_size != n_empty_houses + 1:
-                n_similar = len(np.where(neighborhood == race)[0]) - 1
-                similarity_ratio += n_similar / (neighborhood_size - n_empty_houses - 1.)
-                count += 1
+            if race != 0:
+                neighborhood = self.city[row-self.n_neighbors:row+self.n_neighbors, col-self.n_neighbors:col+self.n_neighbors]
+                neighborhood_size = np.size(neighborhood)
+                n_empty_houses = len(np.where(neighborhood == 0)[0])
+                if neighborhood_size != n_empty_houses + 1:
+                    n_similar = len(np.where(neighborhood == race)[0]) - 1
+                    similarity_ratio += n_similar / (neighborhood_size - n_empty_houses - 1.)
+                    count += 1
         return similarity_ratio / count
 
 
